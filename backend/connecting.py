b18faf244643cf8c6c8f193f30caf3c1da78b9f2
Daniel Banariba <danielbanariba@protonmail.com>
2024-04-02 12:28:56 -0600

    base de datos local

--- backend/connecting.py
+++ backend/connecting.py
@@ -3,14 +3,16 @@ from sqlalchemy import create_engine, text
 
 config = rx.Config(
     app_name="TALLER_MECANICO",
-    db_url="sqlite:///reflex.db",
+    #Base de datos que tenemos en la nube
+    #db_url= "sqlite:///reflex.db",
+    #Base de datos que tenemos de manera local
+    db_url="sqlite:///C:\\Users\\banar\\Desktop\\taller-mecanico\\db\\TALLER_MECANICO.db", 
 )
 
 # Crear un motor de base de datos
 engine = create_engine(config.db_url)
 
 try:
-    # Intentar ejecutar una consulta simple
     with engine.connect() as connection:
         result = connection.execute(text("SELECT 1"))
         print("Conexión exitosa a la base de datos.")
